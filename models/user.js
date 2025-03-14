2e16b66c3a541f0e95a17383859f73b135a2c0b3
abl00mfield <amanda.bloomfield@gmail.com>
2025-03-13 18:42:20 -0600

    completed set up and tested main index. page is functional

--- models/user.js
+++ models/user.js
@@ -1,6 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,8 +16,9 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  pantry: [foodSchema],
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
