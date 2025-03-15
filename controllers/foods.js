1935be2794a01fb102df5dc991f111da09a70fb4
abl00mfield <amanda.bloomfield@gmail.com>
2025-03-15 15:33:31 -0600

    added show and delete route for food

--- controllers/foods.js
+++ controllers/foods.js
@@ -32,4 +32,44 @@ router.get("/new", (req, res) => {
   res.status(200).render("foods/new.ejs");
 });
 
+//SHOW route /users/:userId/foods/:itemId
+router.get("/:foodId", async (req, res) => {
+  try {
+    //look up user
+    const currentUser = await User.findById(req.session.user._id);
+    const food = await currentUser.pantry.id(req.params.foodId);
+    res.render("foods/show.ejs", {
+      food,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+router.delete("/:foodId", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.pantry.id(req.params.foodId).deleteOne();
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/foods`);
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+router.get("/:foodId/edit", async (req, res) => {
+  try {
+    const currentUser = await User.findById(res.session.user._id);
+    const food = currentUser.pantry.id(req.params.foodId);
+    res.render("foods/edit.ejs", {
+      food,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
 module.exports = router;
